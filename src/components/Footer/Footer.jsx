97bb34f5736c0e54098f014bde91e44aa5cbb8d9
Vishesh Mehrotra <meh.vishesh1998@gmail.com>
2023-06-27 11:08:37 +0530

    carousel

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,82 @@
 import React from "react";
 import "./Footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
+import { LiaCopyrightSolid } from "react-icons/lia";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="col">
+          <div className="title">GENERAL</div>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+        </div>
+        <div className="col">
+          <div className="title">YOUR ACCOUNT</div>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+        </div>
+        <div className="col">
+          <div className="title">COLLECTIONS</div>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+          <span className="text">homeee</span>
+        </div>
+      </div>
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+          <div className="text">Product From</div>
+          <div>
+            <BsLinkedin className="icon" />
+
+            <AiFillTwitterCircle className="icon" />
+
+            <BsFacebook className="icon" />
+          </div>
+          <div>
+            <LiaCopyrightSolid className="icon" />
+            <spam>2002</spam>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,72 @@
-import React from "react";
+// import React from "react";
+import "./Main.css";
+
+// const Main = () => {
+//   return <div>Main</div>;
+// };
+
+// export default Main;
+import React, { useState } from "react";
 import "./Main.css";
 
 const Main = () => {
-  return <div>Main</div>;
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const MainData = [
+    {
+      title: "Deal of the Day",
+      subtitle: "Get an extra 30% off",
+      description: "Precision, quality, and longevity",
+    },
+    {
+      title: "SecondDeal of the Day",
+      subtitle: "Get an extra 30% off",
+      description: "Precision, quality, and longevity",
+    },
+    {
+      title: "THird Deal of the Day",
+      subtitle: "Get an extra 30% off",
+      description: "Precision, quality, and longevity",
+    },
+  ];
+
+  const handleNext = () => {
+    setCurrentPage((prevPage) => (prevPage + 1) % MainData.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentPage(
+      (prevPage) => (prevPage - 1 + MainData.length) % MainData.length
+    );
+  };
+
+  return (
+    <div className="Main1">
+      <div
+        className={`Main ${currentPage > 0 ? "Main-transition-left" : ""} ${
+          currentPage < MainData.length - 1 ? "Main-transition-right" : ""
+        }`}
+      >
+        <div className="image-container">
+          <img src="path/to/image.jpg" alt="Main Image" />
+        </div>
+        <div className="content-container">
+          <h2 className="title">{MainData[currentPage].title}</h2>
+          <h3 className="subtitle">{MainData[currentPage].subtitle}</h3>
+          <p className="description">{MainData[currentPage].description}</p>
+          <button className="shop-now-btn">Shop Now</button>
+        </div>
+        <div className="navigation">
+          <button className="arrow-btn prev-btn" onClick={handlePrev}>
+            &#8249;
+          </button>
+          <button className="arrow-btn next-btn" onClick={handleNext}>
+            &#8250;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
